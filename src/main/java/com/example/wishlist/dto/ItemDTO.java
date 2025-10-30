7a0439536ffdc01b536fbcfb600cfba23130c8da
Goose <alshishkov8@gmail.com>
2025-10-30 12:02:12 +0200

    Checkpoint

---
+++ src/main/java/com/example/wishlist/dto/ItemDTO.java
@@ -0,0 +1,50 @@
+package com.example.wishlist.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItemDTO {
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDateTime createdAt;
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
+
